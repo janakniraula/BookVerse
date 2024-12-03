bc213ba0ce6c924b19d5bfca758e15b724a2168f
janak <niraulajanak2019@gmail.com>
2024-12-03 15:29:25 +0545

    Update nav menu for admin

--- lib/navigation_menu/admin_nav.dart
+++ lib/navigation_menu/admin_nav.dart
@@ -9,80 +9,119 @@ import '../features/home/screens/admin/navigations/settingScreen.dart';
 import '../utils/constants/colors.dart';
 import '../utils/helpers/helper_function.dart';
 
-class AdminNavigationMenu extends StatelessWidget {
-  const AdminNavigationMenu({super.key});
+class AdminNavigationController extends GetxController {
+  static AdminNavigationController get instance => Get.find();
+  int selectedIndex = 2;
 
-  @override
-  Widget build(BuildContext context) {
-    final controller = Get.put(AdminNavigationController());
-    final darkMode = THelperFunction.isDarkMode(context);
-
-    return Scaffold(
-      bottomNavigationBar: Obx(
-            () => NavigationBar(
-          height: 65,
-          elevation: 0,
-          selectedIndex: controller.selectedIndex.value,
-          onDestinationSelected: controller.onDestinationSelected,
-          backgroundColor: darkMode
-              ? TColors.black
-              : Colors.white.withOpacity(0.1),
-          indicatorColor: darkMode
-              ? TColors.white.withOpacity(0.3)
-              : TColors.black.withOpacity(0.3),
-          labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
-          destinations: const [
-            NavigationDestination(
-              icon: Icon(Iconsax.book_1),
-              label: 'Add Books',
-            ),
-            NavigationDestination(
-              icon: Icon(Iconsax.edit),
-              label: 'Edit',
-            ),
-            NavigationDestination(
-              icon: Icon(Iconsax.home),
-              label: 'Dashboard',
-            ),
-            NavigationDestination(
-              icon: Icon(Iconsax.message_question),
-              label: 'Requests',
-            ),
-            NavigationDestination(
-              icon: Icon(Iconsax.setting),
-              label: 'Settings',
-            ),
-          ],
-        ),
-      ),
-      body: Obx(
-            () => AnimatedSwitcher(
-          duration: const Duration(milliseconds: 300),
-          child: controller.currentScreen,
-        ),
-      ),
-    );
+  void changeIndex(int index) {
+    selectedIndex = index;
+    update();
   }
 }
 
-class AdminNavigationController extends GetxController {
-  static AdminNavigationController get instance => Get.find();
+class AdminNavigationMenu extends StatelessWidget {
+  const AdminNavigationMenu({super.key});
 
-  final Rx<int> selectedIndex = 2.obs;
-
-  final List<Widget> screens = const [
-    AddBooks(),                  // index 0 - Add Books
-    SearchBookScreen(),          // index 1 - Edit
-    Dashboard(),                 // index 2 - Dashboard (default)
-    AdminUserRequestsScreen(),   // index 3 - Requests
-    AdminSettingsScreen(),       // index 4 - Settings
-  ];
-
-  Widget get currentScreen => screens[selectedIndex.value];
-
-  void onDestinationSelected(int index) {
-    selectedIndex.value = index;
+  NavigationDestination _buildNavDestination(
+    IconData icon,
+    String label,
+    bool isSelected,
+    bool darkMode,
+  ) {
+    return NavigationDestination(
+      icon: Icon(
+        icon,
+        size: 24,
+        color: isSelected
+            ? darkMode
+                ? TColors.white
+                : TColors.primaryColor
+            : darkMode
+                ? TColors.white.withOpacity(0.5)
+                : TColors.darkGrey,
+      ),
+      label: label,
+      selectedIcon: Icon(
+        icon,
+        size: 24,
+        color: darkMode ? TColors.white : TColors.primaryColor,
+      ),
+    );
   }
 
-
+  @override
+  Widget build(BuildContext context) {
+    final darkMode = THelperFunction.isDarkMode(context);
+    
+    return GetBuilder<AdminNavigationController>(
+      init: AdminNavigationController(),
+      builder: (controller) {
+        return Scaffold(
+          bottomNavigationBar: NavigationBarTheme(
+            data: NavigationBarThemeData(
+              labelTextStyle: MaterialStateProperty.all(
+                TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                  color: darkMode ? TColors.white : TColors.black,
+                ),
+              ),
+            ),
+            child: NavigationBar(
+              height: 65,
+              elevation: 0,
+              selectedIndex: controller.selectedIndex,
+              backgroundColor: darkMode ? TColors.black : TColors.white,
+              indicatorColor: darkMode
+                  ? TColors.white.withOpacity(0.1)
+                  : TColors.primaryColor.withOpacity(0.1),
+              onDestinationSelected: controller.changeIndex,
+              destinations: [
+                _buildNavDestination(
+                  Iconsax.book_1,
+                  'Add Books',
+                  controller.selectedIndex == 0,
+                  darkMode,
+                ),
+                _buildNavDestination(
+                  Iconsax.edit,
+                  'Edit',
+                  controller.selectedIndex == 1,
+                  darkMode,
+                ),
+                _buildNavDestination(
+                  Iconsax.home,
+                  'Dashboard',
+                  controller.selectedIndex == 2,
+                  darkMode,
+                ),
+                _buildNavDestination(
+                  Iconsax.message_question,
+                  'Requests',
+                  controller.selectedIndex == 3,
+                  darkMode,
+                ),
+                _buildNavDestination(
+                  Iconsax.setting,
+                  'Settings',
+                  controller.selectedIndex == 4,
+                  darkMode,
+                ),
+              ],
+            ),
+          ),
+          body: IndexedStack(
+            index: controller.selectedIndex,
+            children: const [
+              AddBooks(),
+              SearchBookScreen(),
+              Dashboard(),
+              AdminUserRequestsScreen(),
+              AdminSettingsScreen(),
+            ],
+          ),
+        );
+      },
+    );
+  }
 }
